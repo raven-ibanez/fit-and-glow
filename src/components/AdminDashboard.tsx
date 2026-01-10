cbb13d9b5e7264f00474910f775b25c271aa1f99
Yna <yna@gmail.com>
2026-01-10 17:04:43 +0800

    ghj

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -343,7 +343,7 @@ const AdminDashboard: React.FC = () => {
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === 'slimmetry@Admin!123') {
+    if (password === 'Rspeptide@admin!123') {
       setIsAuthenticated(true);
       localStorage.setItem('peptide_admin_auth', 'true');
       setLoginError('');
